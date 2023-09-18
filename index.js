e47870ca35bed40bcee45e30b2ed82cdfa28362a
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-09-18 14:05:09 -0400

    Add functions for checking if either player won
    Before this change, no one won or tied the game. Now, given a
    randomly generated tic-tac-toe board, a function can determine if
    either the player, or the opponent has won the game in any way
    possible. This function also generates a message indicating who won.
    
    With a randomly generated board, this function is biased for P1
    because the conditional checks P1 first and every value is either
    an X for the player, or an O for the opponent. If the board were
    checked each turn, this function wouldn't have that effect and it
    would work properly.

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 let gameBoard = (function () {
     'use strict';
-    let _gameBoard = [];
-    let _rows = 3;
-    let _columns = 3;
-    for (let i = 0; i < _rows; i++) {
-        _gameBoard[i] = [];
-        for (let j = 0; j < _columns; j++) {
-            _gameBoard[i].push(' ')
-        }
-    };
-    let getBoard = () => _gameBoard;
+    let _fakeChoice = ' ';
+    let _fakeBoard = [
+        //row 1 - 3
+        [_fakeChoice, _fakeChoice, _fakeChoice],
+        [_fakeChoice, _fakeChoice, _fakeChoice],
+        [_fakeChoice, _fakeChoice, _fakeChoice]
+    ]
+    let getBoard = () => _fakeBoard;
 
     //used only in the console version of the game.
-    const printBoard = () => {
-        const boardWithCellValues = _gameBoard.map((row) => row.map((cell) => cell))
-        console.log(boardWithCellValues);
-    };
+    const consoleLogNewBoard = () => {
+        console.log(_fakeBoard.map((row) => row.join(' | ')).join('\n'))
+    }
 
-    return { getBoard, printBoard }
+    return { getBoard, consoleLogNewBoard }
 })();
 
-// gameBoard.printBoard();
+// gameBoard.consoleLogNewBoard();
 
 let fakeChoice = ' ';
 let fakeBoard = [
@@ -29,6 +26,8 @@ let fakeBoard = [
     [fakeChoice, fakeChoice, fakeChoice],
     [fakeChoice, fakeChoice, fakeChoice]
 ]
+
+let getBoard = () => fakeBoard
 
 //makes array of arrays to 3 strings(rows) separated by a newline
 function consoleLogNewBoard(fakeBoard) {
@@ -53,7 +52,7 @@ function opponentsTurn() {
 
 //asks for expected string '1 3'.turns each into their own number placed into their respective variables. puts an "X" into coordinates. Shows new board.
 function playRound() {
-    let tokenPlacement = prompt('Separated by a space, enter the row, then column for your placement');
+    let tokenPlacement = prompt('Separated by a space, enter the row, then column for your token placement');
     let [row, column] = tokenPlacement.split(' ').map(Number)
     coordinatesForChoice(row, column)
     consoleLogNewBoard(fakeBoard)
@@ -61,6 +60,111 @@ function playRound() {
 }
 
 //asks user for first move
-playRound()
+// playRound()
 
 
+
+
+
+
+
+
+
+
+//code challenge #1
+//console game should not allow for player(s) to make a move 
+//on an occupied location
+
+//'2 3'
+//return 'Sorry, location is occupied. Choose another location.'
+
+
+
+
+
+//code challenge #2
+//if input isn't '2 3'
+//return 'Invalid Input. Separated by a space, enter the row, then column for your token placement'
+
+
+
+
+
+//code challenge #3
+//console game should check for 3 in a row
+
+//horizontal win input: [['X', 'X', 'X'],[' ', 'O', ' '],['O', 'O', ' ']]
+//If I want to check if either player won - put: || value === 'O'.
+function isHorizontalWin(gameBoard) {
+    let p1 = gameBoard.some((row) => row.every((value) => value === 'X'))
+    let opponent = gameBoard.some((row) => row.every((value) => value === 'O'))
+    return p1 ? 'player' : opponent ? 'opponent' : false
+}
+console.log(isHorizontalWin([['X', 'X', ' '], ['X', 'O', 'X'], ['O', 'O', 'O']]))
+
+//vertical win input: col 1: [['X', ' ', 'O'],['X', 'O', ' '],['X', 'O', ' ']]
+//If I want to check if either player won vertically add: || value === 'O'
+function isVerticalWin(gameBoard) {
+    let columnOne = [gameBoard[0][0], gameBoard[1][0], gameBoard[2][0]]
+    let columnTwo = [gameBoard[0][1], gameBoard[1][1], gameBoard[2][1]]
+    let columnThree = [gameBoard[0][2], gameBoard[1][2], gameBoard[2][2]]
+    let p1 = [columnOne, columnTwo, columnThree].some((column) => column.every((value) => value === 'X'))
+    let opponent = [columnOne, columnTwo, columnThree].some((column) => column.every((value) => value === 'O'))
+    return p1 ? 'player' : opponent ? 'opponent' : false
+}
+console.log(isVerticalWin([['X', 'O', 'O'], ['X', 'O', ' '], [' ', 'O', ' ']]))
+
+//diagonal win input: [['X', ' ', 'O'],[' ', 'X', ' '],['O', 'O', 'X']]
+function isDiagonalWin(gameBoard) {
+    //diagonal arrays
+    let topLeftBottomRight = [gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]]
+    let bottomLeftTopRight = [gameBoard[2][0], gameBoard[1][1], gameBoard[0][2]]
+    let p1 = [topLeftBottomRight, bottomLeftTopRight].some((diagonal) => diagonal.every((value) => value === 'X'))
+    let opponent = [topLeftBottomRight, bottomLeftTopRight].some((diagonal) => diagonal.every((value) => value === 'O'))
+    return p1 ? 'player' : opponent ? 'opponent' : false
+}
+console.log(isDiagonalWin([[' ', ' ', 'X'], [' ', 'X', ' '], ['X', 'O', ' ']]))
+
+//from a randomly generated board, check if X won and console.log the board.
+function checkWhoWon(gameBoard) {
+    let opponent = 'CPU';
+    if (isHorizontalWin(gameBoard) === 'player' || isVerticalWin(gameBoard) === 'player' || isDiagonalWin(gameBoard) === 'player') {
+        return 'P1 WON'
+    } else if (isHorizontalWin(gameBoard) === 'opponent' || isVerticalWin(gameBoard) === 'opponent' || isDiagonalWin(gameBoard) === 'opponent') {
+        return `${opponent} WON`
+    } else {
+        return 'TIE'
+    }
+}
+
+//alternate X's and O's for values in randomly generated board
+function generateRandomBoard(gameBoard) {
+    let options = {
+        0: 'X',
+        1: 'O',
+    }
+    let xCount = 0;
+    let oCount = 0;
+    while (gameBoard.some((row) => row.some((value) => value === ' '))) {
+        let empty = ' ';
+        let input;
+        let [coordinate1, coordinate2] = [Math.floor(Math.random() * 3), Math.floor(Math.random() * 3)];
+        while (gameBoard[coordinate1][coordinate2] !== empty) {
+            coordinate1 = Math.floor(Math.random() * 3)
+            coordinate2 = Math.floor(Math.random() * 3)
+        }
+        if (xCount > oCount) {
+            input = 'O';
+        } else if (xCount < oCount) {
+            input = 'X';
+        } else {
+            input = options[Math.floor(Math.random() * 2)]
+        }
+        input === 'X' ? xCount++ : oCount++
+        gameBoard[coordinate1][coordinate2] = input
+    }
+    return gameBoard
+}
+
+let newBoard = generateRandomBoard(fakeBoard)
+console.log(newBoard, checkWhoWon(newBoard))
